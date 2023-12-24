f73ad83b9c130d55614823b15f6679222f6dcc1c
Nadeem Samaali <nadeem.samaali@gmail.com>
2023-12-23 20:34:28 -0500

    Added printMatrix and setMatrix methods

--- Determinant/src/App.java
+++ Determinant/src/App.java
@@ -6,30 +6,32 @@ public class App {
     static Scanner input = new Scanner(System.in);
 
     /**
-     * 'printMatrix' is a method which allows for printing of any n sized matrix based on the entries of the user
+     * This method allows for printing of any n sized 
+     * square matrix based on the entries of the user
      * 
-     * @param sCounter Counter used for selecting the data entered by the user
-     * @param mSize Size of the matrix to be printed
+     * @param mSize1 Size of the matrix to be printed
      * @param ansArray Array of the entries entred by the user
      */
 
-    public static void printMatrix(int sCounter, int mSize, String[] ansArray)
+    public static void printMatrix(int mSize1, String[] ansArray1)
     {
+        int sCounter1 = -1;
+
         //Outputting the matrix as well as the data entries
         System.out.print("[");
-        for(int a = 0; a<=mSize; a++)
+        for(int a = 0; a<=mSize1; a++)
         {
             System.out.print("[");
-            for(int b = 0; b<mSize; b++)
+            for(int b = 0; b<mSize1; b++)
             {
-                sCounter += 1;
-                System.out.print(ansArray[sCounter]);
+                sCounter1 += 1;
+                System.out.print(ansArray1[sCounter1]);
                 System.out.print(",");
             }
-            sCounter += 1;
-            System.out.print(ansArray[sCounter]);
+            sCounter1 += 1;
+            System.out.print(ansArray1[sCounter1]);
 
-            if(sCounter == Math.pow(mSize+1,2)-1)
+            if(sCounter1 == Math.pow(mSize1+1,2)-1)
             System.out.print("]");
 
             else
@@ -38,48 +40,57 @@ public class App {
         System.out.println("]");
     }
 
+    /***
+     * This method will input the values of the entires in their designated
+     * location withing the matrix array
+     * 
+     * @param mSize2 Size of the matrix array
+     * @param ansArray2 Array of the entires of the user
+     */
+
+    public static void setMatrix(int mSize2, String[] ansArray2)
+    {
+
+        int sCounter2 = -1;
+
+        double[][] m = new double[mSize2+1][mSize2+1];
+
+        for(int i = 0; i<=mSize2; i++)
+        {
+            for(int j = 0; j<=mSize2; j++)
+            {
+                sCounter2 += 1;
+                m[i][j] = Double.valueOf(ansArray2[sCounter2]);
+            }
+            
+        }
+    }
+
+
     public static void main(String[] args) throws Exception {
 
         //Inputing the value of the size of matrix m
-        System.out.println("\n>> Please insert matrix size (1,2,3...,n) :\n");
+        System.out.println("\n>> Please insert square matrix size (1,2,3...,n) :\n");
         String ans;
         ans = input.nextLine();
 
         //Setting the value n as the size of the matrix
         int n = Integer.valueOf(ans)-1;
-
-
-        //Creating a new square matrix based on size 'n'
-        double[][] m = new double[n][n];
         ans = "";
 
-        //Enetring the entries of the matrix 
-        System.out.println("\n>> Insert the values of the entries sparated with a coma\n   respecting this form : a11,...,a1n,...,am1,...,amn\n");
+        //Adding the entries of the matrix into an array by splitting
+        System.out.println("\n>> Insert the values of the entries sparated with a comma\n   respecting this form : a11,...,a1n,...,am1,...,amn\n");
         ans = input.nextLine();
+        String[] e = ans.split(",");
 
-        //Outputting the matrix as well as the data entries
+        //Outputting the matrix
         System.out.print("\n>> Here is the inputted matrix : ");
-        printMatrix(-1, n, ans.split(","));
+        printMatrix(n,e);
         System.out.println();
-    
-        /*
-        int matrixSize;
-        matrixSize=-1;
 
-        //Filling the matrix with the numbers inputted
-        for(int i = 0; i<=n; i++)
-        {
-            for(int j = 0; j<=n; j++)
-            {
-                matrixSize += 1;
+        //Set the entires into the designated placements in the matrix Array
+        setMatrix(n,e);
 
-                m[i][j] = Double.valueOf(ansEntry[matrixSize]);
-                System.out.println(m[i][j]);
-
-            }
-            
-        }
-        */
 
     }
 
