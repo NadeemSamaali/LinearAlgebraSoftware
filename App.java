519f0fcd7839567392a4cc1e2cf37c9b4f942554
Nadeem Samaali <nadeem.samaali@gmail.com>
2024-01-10 23:16:08 -0500

    1.3.2 | Implemented the new matrix set method

--- App.java
+++ App.java
@@ -15,7 +15,7 @@ import vectorOperations.vOPS;
  *      - Calculating the dot product of two vectors of n dimension (1.3.0)
  *
  * @author Nadeem Samaali
- * @version 1.3.1 - Bug Fixing
+ * @version 1.3.2 - Implementation of the new matrix set method
  */
 
 
@@ -64,37 +64,33 @@ public class App {
                         //Inputing the value of the size of matrix m
                             System.out.println("\n1) Please insert the square matrix size (1,2,3...,n) :\n");
                             ans = "";
-                            System.out.print("U : ");
+                            System.out.print("   N = ");
                             ans = input.nextLine();
 
                             //Setting the value n as the size of the matrix
                             n = Integer.valueOf(ans)-1;
-                            //ans = "";
-
-                            //Adding the entries of the matrix into an array by splitting
-                            System.out.println("\n2) Insert the values of the entries sparated with a comma\n   respecting this form : a11,...,a1n,...,am1,...,amn");
-                            System.out.print("\nU : ");
-                            ans = input.nextLine();
-                            entries = ans.split(",");
-
-                            //Outputting the matrix
-                            System.out.print("\n-  Here is the inputted matrix : ");
-                            mOPS.printEntries(n,entries);
-                            System.out.println();
 
                             double[][] m1 = new double[n+1][n+1];
 
+                            //Adding the entries of the matrix into an array by splitting
+                            System.out.println("\n2) Insert the values of the entries by row, with each value seperated by a space\n   respecting this form :\n\n   a11 a12 ... a1n\n   a21 a22 ... a2n\n   a1n a2n ... amn\n");
+                            mOPS.setMatrix(m1,n,input);
+                            
+                            //Outputting the matrix
+                            System.out.print("\n-  Here is the inputted matrix : ");
+                            //mOPS.printEntries(n,entries);
+                            System.out.println();
                             //Set the entires into the designated placements in the matrix Array
-                            mOPS.setMatrixFromString(m1,n,entries);
                             mOPS.printMatrix(m1, n);
-
-                            //Reduce the matrix into upper-triangular form
+                            //Reduce the matrix into upper-triangular form and calculating the determinant
                             detFinder.getDeterminant(m1, n);
+                            
+                                
                         break;
 
                         case "#getInverse" :
                             System.out.println("\n1) Please enter the square matrix size (1,2,...,n) : \n");
-                            System.out.print("U : ");
+                            System.out.print("   N = ");
                             Scanner in = new Scanner(System.in);
                             ans = in.nextLine();
 
@@ -103,34 +99,39 @@ public class App {
                             double[][] m2 = new double[n+1][n+1];
                             double[][] d = new double[n+1][n+1];
 
-                            System.out.println("\n2) Insert the values of the entries sparated with a comma\n   respecting this form : a11,...,a1n,...,am1,...,amn\n");
-                            System.out.print("U : ");
-                            ans = in.nextLine();
-                            entries = ans.split(",");
+                            System.out.println("\n2) Insert the values of the entries by row, with each value seperated by a space\n   respecting this form :\n\n   a11 a12 ... a1n\n   a21 a22 ... a2n\n   a1n a2n ... amn\n");
+                            
+                            mOPS.setMatrix(m2,n,input);
 
-                            mOPS.setMatrixFromString(m2, n, entries);
-                            mOPS.setMatrixFromString(d, n, entries);
+                            //Setting up the mock matrix 
+                            for(int x = 0; x<=n; x++) {
+                                for(int y = 0; y<=n; y++) {
+                                    d[x][y] = m2[x][y];
+                                }
+                            }
+
+                       
                             double detM = detFinder.getSilentDeterminant(d,n);
 
-                            if(detM != 0)
-                            {
+                            if(detM != 0){
                                 inverseFinder.getCofactorMatrix(m2, n);
                                 inverseFinder.getAdjacentMatrix(m2,n);
                                 inverseFinder.getInverse(m2, n, detM);
-                                System.out.print("\nThe inverse of the inputted matrix is : ");
+                                System.out.print("\n>> The inverse of the inputted matrix is : ");
                                 mOPS.printMatrix(m2,n);
                             }
                             
-                        else
-                        {
-                            System.out.println("\nERROR : This matrix is not invertible !");
-                        }
+                            else
+                            {
+                                System.out.println(">>\nERROR : This matrix is not invertible !");
+                            }
                         break;
 
                         case "#findX" :
                             System.out.println("\n1) A - Please insert the square coefficient matrix size (1,2,3...,n) :\n");
                             ans = "";
-                            System.out.print("U : ");
+
+                            System.out.print("   N = ");
                             ans = input.nextLine();
                             n = Integer.valueOf(ans) - 1;
                     
@@ -138,14 +139,11 @@ public class App {
                             double[] b = new double[n+1];
                     
                     
-                            System.out.println("\n2) A - Insert the values of the entries sparated with a comma\n   respecting this form : a11,...,a1n,...,am1,...,amn\n");
-                            System.out.print("U : ");
-                            String ans1 = input.nextLine();
+                            System.out.println("\n2) Insert the values of the entries by row, with each value seperated by a space\n   respecting this form :\n\n   a11 a12 ... a1n\n   a21 a22 ... a2n\n   a1n a2n ... amn\n");
+                            mOPS.setMatrix(m3,n,input);
 
-                            mOPS.setMatrixFromString(m3, n, ans1.split(","));
-
-                            System.out.println("\n3) B - Insert the values of the solution sparated with a comma\n   respecting this form : b1,b2,...,bn\n");
-                            System.out.print("U : ");
+                            System.out.println("\n3) B - Insert the values of the solution sparated with a space\n   respecting this form : b1 b2 ... bn\n");
+                            System.out.print("   ");
                             String ans2 = input.nextLine();
 
                             findX.setB(b, ans2, n);
@@ -154,19 +152,19 @@ public class App {
 
                         case "#dotProduct":
 
-                            System.out.println("\n1) Insert the components of V1 (v11, v12,..., v1n)\n");
-                            System.out.print("U : ");
+                            System.out.println("\n1) Insert the components of V1 separated by a space (v11 v12 ... v1n)\n");
+                            System.out.print("   ");
                             ans = input.nextLine();
-                            String[] E1 = ans.split(",");
+                            String[] E1 = ans.split(" ");
                             double[] V1 = new double[E1.length];
-                            vOPS.setVectorFromString(V1, E1);
+                            vOPS.setVector(V1, E1);
 
-                            System.out.println("\n2) Insert the components of V2 (v21, v22,..., v2n)\n");
-                            System.out.print("U : ");                            
+                            System.out.println("\n2) Insert the components of V2 separated by a space (v21 v22 ... v2n)\n");
+                            System.out.print("   ");                            
                             ans = input.nextLine();
-                            String[] E2 = ans.split(",");
+                            String[] E2 = ans.split(" ");
                             double[] V2 = new double[E2.length];
-                            vOPS.setVectorFromString(V2, E2);
+                            vOPS.setVector(V2, E2);
 
                             if(V1.length == V2.length)
                             {
@@ -193,7 +191,7 @@ public class App {
 
             catch(Exception e)
             {
-                System.out.println("\n>> ERROR : illegal prompt - Please start again");
+                System.err.println("\n>> ERROR : illegal prompt - " + e.getMessage());
             }
 
         }while(!key);
